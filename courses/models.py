11bbe7399cd577044f9af28a629c60a3c2a89152
SharequeKhan77 <sharequekhan25@gmail.com>
2026-06-01 16:38:49 +0530

    Initial LearnHub setup

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateField(auto_now_add=True)
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class Enrollment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    enrolled_date = models.DateField(auto_now_add=True)
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Category, Course, Lesson, Enrollment
 
 # Create your views here.
+def index(request):
+    courses = Course.objects.all()
+    return render(request, 'courses/index.html', {'courses': courses})
