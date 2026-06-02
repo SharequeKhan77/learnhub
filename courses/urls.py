4d3c536c97804fce15cd5e9fce9ef4ab07174459
SharequeKhan77 <sharequekhan25@gmail.com>
2026-06-02 18:32:46 +0530

    Add views, URLs and login configuration

---
+++ courses/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('course/<int:id>/', views.course_detail, name='view_course'),
+    path('enroll/<int:id>/', views.enroll, name='view_enroll'),
+    path('my-courses/', views.my_courses, name='my_courses')
+]
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Course, Lesson, Enrollment
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/login/')
 def index(request):
     courses = Course.objects.all()
     return render(request, 'courses/index.html', {'courses': courses})
+
+@login_required(login_url='/login/')
+def course_detail(request, id):
+    course = Course.objects.get(id=id)
+    lessons = Lesson.objects.filter(course=course)
+    return render(request, 'courses/course_detail.html', {'course': course, 'lessons': lessons})
+
+@login_required(login_url='/login/')
+def enroll(request, id):
+    course = Course.objects.get(id=id)
+    Enrollment.objects.create(user=request.user, course=course)
+    return redirect('my_courses')
+
+@login_required(login_url='/login/')
+def my_courses(request):
+    enrollments = Enrollment.objects.filter(user=request.user)
+    return render(request, 'courses/my_courses.html', {'enrollments': enrollments})
